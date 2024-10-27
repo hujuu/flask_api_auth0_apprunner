2addc9b6d1bec664186b1be942a8a50928b8d570
hujuu <1961946+hujuu@users.noreply.github.com>
2024-10-28 01:16:11 +0900

    Add speakers endpoint

--- main.py
+++ main.py
@@ -202,6 +202,16 @@ def venues():
     return jsonify({"id":1,"name":"Auditorium A"}), 404
 
 
+@app.route("/api/v1/speakers")
+def speakers():
+    firstname = request.args.get("firstname")
+    lastname = request.args.get("lastname")
+    if firstname is not None and lastname is not None:
+        return jsonify(message="The speaker's fullname :" + firstname+" "+ lastname)
+    else:
+        return jsonify(message="No query parameters in the url")
+
+
 @app.route("/api/public")
 @cross_origin(headers=["Content-Type", "Authorization"])
 def public():
